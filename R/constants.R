63f38f31e87ba163732bc9571a25b232e7a32e1b
Pedro Costa <pedro.mpm.costa@gmail.com>
2018-03-06 20:00:01 +0000

    Added new format string for invalid timestamp

--- R/constants.R
+++ R/constants.R
@@ -2,3 +2,9 @@
 #' @usage .FMT.CLS.MIS
 .FMT.CLS.MIS <- "class mismatch: expected 'exphist', found '%s'"
 names(.FMT.CLS.MIS) <- '.FMT.CLS.MIS'
+
+#' Format string for invalid timestampt
+#' @usage .FMT.INV.TSP
+.FMT.INV.TSP <- "invalid timestamp: %d>=%d"
+names(.FMT.INV.TSP) <- '.FMT.INV.TSP'
+
