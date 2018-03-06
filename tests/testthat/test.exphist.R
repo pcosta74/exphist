4e398dc0584257647ed19d74918fe5e83e69b6e5
Pedro Costa <pedro.mpm.costa@gmail.com>
2018-03-06 20:02:30 +0000

    new tests for expiration date

--- tests/testthat/test.exphist.R
+++ tests/testthat/test.exphist.R
@@ -3,10 +3,14 @@ context('exphist')
 test_that('exphist returns an objet of class "exphist"', {
   expect_error(exphist(NULL), 'is.numeric\\(winsize\\) && winsize > 1 is not TRUE')
   expect_error(exphist(winsize=NULL), 'is.numeric\\(winsize\\) && winsize > 1 is not TRUE')
-  expect_error(exphist(epson=NULL), 'is.numeric\\(epson\\) && 0 < epson && epson < 1 is not TRUE')
+  expect_error(exphist(epsilon=NULL), 'is.numeric\\(epsilon\\) && 0 < epsilon && epsilon < 1 is not TRUE')
   expect_error(exphist(1,1), 'is.numeric\\(winsize\\) && winsize > 1 is not TRUE')
-  expect_error(exphist(2,1), 'is.numeric\\(epson\\) && 0 < epson && epson < 1 is not TRUE')
-  expect_s3_class(exphist(2,0.05), 'exphist')
+  expect_error(exphist(2,1), 'is.numeric\\(epsilon\\) && 0 < epsilon && epsilon < 1 is not TRUE')
+  expect_error(exphist(2,1,'0'), 'is.null\\(duration\\) || \\(is.numeric\\(duration\\) && duration > 0\\) is not TRUE')
+  expect_error(exphist(2,1,0), 'is.null\\(duration\\) || \\(is.numeric\\(duration\\) && duration > 0\\) is not TRUE')
+  expect_s3_class(exphist(2, 0.05), 'exphist')
+  expect_s3_class(exphist(2, 0.05, NULL), 'exphist')
+  expect_s3_class(exphist(2, 0.05, 1), 'exphist')
 })
 
 test_that('insert yields an objet of class "exphist"', {
@@ -21,4 +25,11 @@ test_that('value yields the correct sum of bucket values', {
   eh <- insert(eh, 1)
   eh <- insert(eh, 1)
   expect_equal(value(eh), 2)
+})
+
+test_that('time-based exphist yield the correct value and size', {
+  eh <- exphist(duration=5)
+  for(t in 1:6) eh <- insert(eh, 1, t)
+  expect_equal(value(eh), 6)
+  expect_equal(size(eh), 2)
 })
